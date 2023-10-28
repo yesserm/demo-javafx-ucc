17fa887c2e2b70e71f0987f367fced92697fd9cb
yesserm <yesser.miranda@uraccan.edu.ni>
2023-10-28 02:26:45 -0600

    Validando el inicio de sesion

--- src/main/java/org/yesser/controller/PrincipalController.java
+++ src/main/java/org/yesser/controller/PrincipalController.java
@@ -2,16 +2,68 @@ package org.yesser.controller;
 
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
 import org.yesser.model.User;
 import org.yesser.service.GenericServiceImpl;
 import org.yesser.service.IGenericService;
 import org.yesser.utils.Global;
 import org.yesser.utils.HibernateUtil;
 
-public class PrincipalController {
-    public void ingresarAlSistema(ActionEvent actionEvent) {
-        System.out.println("Ingresando al sistema");
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PrincipalController implements Initializable {
+    @FXML
+    TextField txtPassword;
+    @FXML TextField txtUser;
+    User usuario;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        usuario = new User();
         crearUsuario();
+    }
+
+    public void ingresarAlSistema(ActionEvent actionEvent) {
+        if (txtUser.getText().isEmpty() || txtPassword.getText().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Error al iniciar sesion");
+            alert.setContentText("Por favor, complete todos los campos");
+            alert.showAndWait();
+        } else {
+            System.out.println("Ingresando al sistema");
+            String nickUser = txtUser.getText();
+            String claveUser = txtPassword.getText();
+
+            if (getUsuariobyNick(nickUser,claveUser)){
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle("Informacion");
+                alert.setHeaderText("Inicio de sesion completado");
+                alert.setContentText("Abriendo el sistema");
+                alert.showAndWait();
+            } else {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText("Error al iniciar sesion");
+                alert.setContentText("Datos incorrectos");
+                alert.showAndWait();
+            }
+        }
+    }
+
+    public boolean getUsuariobyNick(String nick, String password) {
+        ObservableList<User> usuarios = Global.getUsuarios();
+        for (User us : usuarios) {
+            if (us.getNick().equals(nick) && us.getClave().equals(password)) {
+                this.usuario = us;
+                return true;
+            }
+        }
+        return false;
     }
 
     public void mostrarInicio(ActionEvent actionEvent) {
